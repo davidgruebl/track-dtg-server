01ffaa944c6e4f4e71d5b6ac2c06564f55456bd9
David Thomas Grübl <me@davidgruebl.com>
2015-02-13 15:18:42 +0100

    chore

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var locations = require('./routes/location')
+var app = express()
+
+var dbName = 'locationsDB'
+var connectionString = 'mongodb://localhost:27017/' + dbName
+
+mongoose.connect(connectionString)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api', locations)
+
+module.exports = app
