b953f52a53028f18d33c920e713a68c16990fd5c
David Thomas Grübl <me@davidgruebl.com>
2015-02-13 21:15:06 +0100

    chore: initialize passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var passport = require('passport')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var locations = require('./routes/location')
@@ -10,7 +11,9 @@ var connectionString = 'mongodb://localhost:27017/' + dbName
 mongoose.connect(connectionString)
 
 app.use(bodyParser.json())
+app.use(passport.initialize())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api', locations)
 
 module.exports = app
+
