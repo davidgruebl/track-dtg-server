cc66edbcbf1e764a637fb70b2d2383820990db6c
David Thomas Grübl <me@davidgruebl.com>
2015-02-26 20:55:50 +0100

    feat: simplify restart script

--- bin/restart.sh
+++ bin/restart.sh
@@ -6,4 +6,4 @@ export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "/root/.nvm/nvm.sh"
 nvm use iojs
 
-forever list | grep "bin/www.js" | awk '{print $2}' | xargs node -e "console.log(process.argv[1].replace(/[\[\]]/g,''))" | xargs forever restart
+forever restartall
